48e36b3883e7c06f34d857185c1912a33d50ab53
Patrick Nguyen <iamtiredofyou2@gmail.com>
2022-06-10 14:34:27 -0400

    Refactored the Table component and created the TableRow component

--- components/Table/Table.jsx
+++ components/Table/Table.jsx
@@ -1,87 +1,31 @@
-import { HiOutlineCheck } from 'react-icons/hi';
-import tw from 'twin.macro';
+import 'twin.macro';
+
+import TableRow from '../TableRow/TableRow';
 
 const Table = ({ plans, selectedPlan }) => {
   return (
     <table>
       <tbody tw='divide-y divide-[gray]'>
-        <tr tw='flex flex-wrap items-center font-medium'>
-          <td tw='w-full p-2.5 text-center text-sm font-normal text-white md:w-2/5 md:p-3.5 md:text-left md:text-base'>
-            Monthly price
-          </td>
-          {plans.map(plan => (
-            <td
-              key={plan.id}
-              tw='w-[calc(100%/3)] p-2.5 text-center md:w-[calc(60%/3)] md:p-3.5'
-              css={
-                selectedPlan.id === plan.id
-                  ? tw`text-[#E50914]`
-                  : tw`text-[gray]`
-              }
-            >
-              ${plan.prices[0].unit_amount / 100}
-            </td>
-          ))}
-        </tr>
-
-        <tr tw='flex flex-wrap items-center font-medium'>
-          <td tw='w-full p-2.5 text-center text-sm font-normal text-white md:w-2/5 md:p-3.5 md:text-left md:text-base'>
-            Video quality
-          </td>
-          {plans.map(plan => (
-            <td
-              key={plan.id}
-              tw='w-[calc(100%/3)] p-2.5 text-center md:w-[calc(60%/3)] md:p-3.5'
-              css={
-                selectedPlan.id === plan.id
-                  ? tw`text-[#E50914]`
-                  : tw`text-[gray]`
-              }
-            >
-              {plan.metadata.videoQuality}
-            </td>
-          ))}
-        </tr>
-
-        <tr tw='flex flex-wrap items-center font-medium'>
-          <td tw='w-full p-2.5 text-center text-sm font-normal text-white md:w-2/5 md:p-3.5 md:text-left md:text-base'>
-            Resolution
-          </td>
-          {plans.map(plan => (
-            <td
-              key={plan.id}
-              tw='w-[calc(100%/3)] p-2.5 text-center md:w-[calc(60%/3)] md:p-3.5'
-              css={
-                selectedPlan.id === plan.id
-                  ? tw`text-[#E50914]`
-                  : tw`text-[gray]`
-              }
-            >
-              {plan.metadata.resolution}
-            </td>
-          ))}
-        </tr>
-
-        <tr tw='flex flex-wrap items-center font-medium'>
-          <td tw='w-full p-2.5 text-center text-sm font-normal text-white md:w-2/5 md:p-3.5 md:text-left md:text-base'>
-            Watch on your TV, computer, mobile phone and tablet
-          </td>
-          {plans.map(plan => (
-            <td
-              key={plan.id}
-              tw='w-[calc(100%/3)] p-2.5 text-center md:w-[calc(60%/3)] md:p-3.5'
-              css={
-                selectedPlan.id === plan.id
-                  ? tw`text-[#E50914]`
-                  : tw`text-[gray]`
-              }
-            >
-              {plan.metadata.portability === 'true' && (
-                <HiOutlineCheck tw='h-8 w-8 inline-block' />
-              )}
-            </td>
-          ))}
-        </tr>
+        <TableRow
+          title='Monthly price'
+          plans={plans}
+          selectedPlan={selectedPlan}
+        />
+        <TableRow
+          title='Video quality'
+          plans={plans}
+          selectedPlan={selectedPlan}
+        />
+        <TableRow
+          title='Resolution'
+          plans={plans}
+          selectedPlan={selectedPlan}
+        />
+        <TableRow
+          title='Watch on your TV, computer, mobile phone and tablet'
+          plans={plans}
+          selectedPlan={selectedPlan}
+        />
       </tbody>
     </table>
   );
---
+++ components/TableRow/TableRow.jsx
@@ -0,0 +1,35 @@
+import tw from 'twin.macro';
+import { HiOutlineCheck } from 'react-icons/hi';
+
+const TableRow = ({ title, plans, selectedPlan }) => {
+  return (
+    <tr tw='flex flex-wrap items-center font-medium'>
+      <td tw='w-full p-2.5 text-center text-sm font-normal text-white md:w-2/5 md:p-3.5 md:text-left md:text-base'>
+        {title}
+      </td>
+      {plans.map(plan => (
+        <td
+          key={plan.id}
+          tw='w-[calc(100%/3)] p-2.5 text-center md:w-[calc(60%/3)] md:p-3.5'
+          css={
+            selectedPlan.id === plan.id ? tw`text-[#E50914]` : tw`text-[gray]`
+          }
+        >
+          {
+            {
+              'Monthly price': `$${plan.prices[0].unit_amount / 100}`,
+              'Video quality': plan.metadata.videoQuality,
+              Resolution: plan.metadata.resolution,
+              'Watch on your TV, computer, mobile phone and tablet': plan
+                .metadata.portability === 'true' && (
+                <HiOutlineCheck tw='h-8 w-8 inline-block' />
+              )
+            }[title]
+          }
+        </td>
+      ))}
+    </tr>
+  );
+};
+
+export default TableRow;
